dfbbee07ef43ef5cb7e78bfaf44e5f26614c5ffe
Mel DeJesus <meldejesus@mels-mbp.attlocal.net>
2017-12-04 20:53:05 -0500

    wow

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 module.exports = {
 
-  
-function printName() {
-var jack=document.getElementById("a");
-jack.onclick = function change() {
+    jack: document.getElementById("a").onclick = function change() {
     jack.innerHTML="WOW";
 }
 } 
